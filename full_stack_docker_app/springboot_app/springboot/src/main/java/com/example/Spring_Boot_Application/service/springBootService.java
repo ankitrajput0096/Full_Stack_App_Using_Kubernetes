77f14ffa124a7798bc765395fecb7c329f75d30a
Ankit_Rajput <ankitrajput0096@gmail.com>
2021-01-12 13:12:16 +0530

    adding liveness and readiness probes

--- full_stack_docker_app/springboot_app/springboot/src/main/java/com/example/Spring_Boot_Application/service/springBootService.java
+++ full_stack_docker_app/springboot_app/springboot/src/main/java/com/example/Spring_Boot_Application/service/springBootService.java
@@ -1,6 +1,8 @@
 package com.example.Spring_Boot_Application.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.Spring_Boot_Application.model.Topic;
@@ -8,28 +10,29 @@ import com.example.Spring_Boot_Application.repository.TopicRepository;
 import com.example.Spring_Boot_Application.serviceInterface.SpringBootServiceInter;
 
 @Service
-public class springBootService implements SpringBootServiceInter{
-	@Autowired
-	private TopicRepository topicRepository;
-	public List<Topic> getAllTopics()
-	{
-		return topicRepository.findAll();
-	}
-	public Topic getTopic(String id)
-	{
-		return topicRepository.findOne(id);
-	}
-	public void addTopic(Topic topic)
-	{
-		topicRepository.save(topic);
-	}
-	public void updatetopic(Topic topic,String id)
-	{
-		topicRepository.delete(id);
-		topicRepository.save(topic);
-	}
-	public void deletetopic(String id)
-	{
-		topicRepository.delete(id);
-	}	
+public class springBootService implements SpringBootServiceInter {
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public Topic getTopic(String id) {
+        Optional<Topic> topicEntity = topicRepository.findById(id);
+        return topicEntity.isPresent() ? topicEntity.get() : null;
+    }
+
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updatetopic(Topic topic, String id) {
+        topicRepository.deleteById(id);
+        topicRepository.save(topic);
+    }
+
+    public void deletetopic(String id) {
+        topicRepository.deleteById(id);
+    }
 }
